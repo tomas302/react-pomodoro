80caf026214fcda6429f1bdcc886f58a51d92a9c
Thomas Dornell <thomasdornell@gmail.com>
2019-01-28 10:49:15 +0100

    Finished design

--- src/Timer.js
+++ src/Timer.js
@@ -1,27 +1,31 @@
 import React, { Component } from 'react';
 
+const playButton = <i title="Start" class="fas fa-play"></i>;
+const pauseButton = <i title="Stop" class="fas fa-pause"></i>;
+
 class TimerLengthControl extends Component {
     render() {
         return <div id="timer-length">
             <div class="length-control">
                 <div id="break-label">Break Length</div>
-                <button id="break-decrement">-</button>
+                <button id="break-decrement"><i class="fas fa-arrow-down"></i></button>
                 <div id="break-length">5</div>
-                <button id="break-increment">+</button>
+                <button id="break-increment"><i class="fas fa-arrow-up"></i></button>
             </div>
             <div class="length-control">
                 <div id="session-label">Session Length</div>
-                <button id="session-decrement">-</button>
+                <button id="session-decrement"><i class="fas fa-arrow-down"></i></button>
                 <div id="session-length">25</div>
-                <button id="session-increment">+</button>
+                <button id="session-increment"><i class="fas fa-arrow-up"></i></button>
             </div>
         </div>;
     }
 }
 
 export default class Timer extends Component {
+    
     render() {
-        return <div>
+        return <div id="timer-wrapper">
             <h1>Pomodoro Clock</h1>
             <TimerLengthControl />
             <div id="timer">
@@ -29,9 +33,10 @@ export default class Timer extends Component {
                 <div id="time-left">
                     25:00
                 </div>
-                <button id="start_stop">Start</button>
-                <button id="reset">Reset</button>
             </div>
+            <button id="start_stop"><i title="Start" class="fas fa-play"></i></button>
+            <button id="reset"><i title="Reset" class="fas fa-sync-alt"></i></button>
+            <div id="footer">Coded by <a href="https://github.com/tomas302">tomas302</a></div>
         </div>;
     }
 }
